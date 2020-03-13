a29508dcfdda2fcdbe4653b890bfa6f537dac747
ph3nd <brendan.cale87@gmail.com>
2020-03-13 16:23:43 +0800

    More models and initial migrations

--- GardenTracker/models.py
+++ GardenTracker/models.py
@@ -1,43 +1,50 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class Seed(models.Model):
-    class HarvestChoices(models.TextChoice):
+    class HarvestChoices(models.TextChoices):
         HARVEST_SINGLE = 1, _('Single Harvest')
         HARVEST_CONTINUOUS = 2, _('Continuous Harvest')
         HARVEST_MULTIPLE = 3, _('Multiple Harvest')
 
-    class PreferredSun(models.TextChoice):
+    class PreferredSun(models.TextChoices):
         SUN_FULL = 1, _('Full Sun')
         SUN_PART = 2, _('Sun/Shade')
         SUN_SHADE = 3, _('Shade')
 
-    common_name = models.CharField()
-    botanical_name = models.CharField()
+    common_name = models.CharField(max_length=256)
+    botanical_name = models.CharField(max_length=256)
     description = models.TextField()
     germination_days = models.IntegerField()
     pot_on_days = models.IntegerField()
     transplant_days = models.IntegerField()
     first_harvest_days = models.IntegerField()
     harvest_type = models.IntegerField(choices=HarvestChoices.choices, default=HarvestChoices.HARVEST_SINGLE)
-    prefered_sun = models.IntegerField(choices=PreferedSun.choices, default=PreferedSun.SUN_FULL)
+    preferred_sun = models.IntegerField(choices=PreferredSun.choices, default=PreferredSun.SUN_FULL)
 
 
 class Plant(models.Model):
     
     seed = models.ForeignKey('Seed', on_delete=models.PROTECT)
     soil_description = models.TextField()
+    quantity = models.IntegerField()
     sow_date = models.DateField()
     germinate_date = models.DateField()
     pot_on_date = models.DateField()
     transplant_date = models.DateField()
     first_harvest_date = models.DateField()
     last_harvest_date = models.DateField()
+    harvested = models.BooleanField(default=False)
+    total_yield = models.IntegerField(help_text='Yield in KG')
 
 
 class Note(models.Model):
 
     seed_id = models.ForeignKey('Seed', on_delete=models.CASCADE)
     plant_id = models.ForeignKey('Plant', on_delete=models.CASCADE)
+    note = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to='notes/')
 
